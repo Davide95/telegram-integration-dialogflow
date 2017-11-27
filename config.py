c91795fbef16cd8f4f238b62519d0d7b0f5ca78b
Davide Riva <driva95@protonmail.com>
2017-11-27 23:51:46 +0100

    Better config file

--- config.py
+++ config.py
@@ -1,8 +1,10 @@
-# Logging config
+# Logging configs
 import logging
-logging.basicConfig(format='%(asctime)s - %(levelname)s: %(message)s.', level=logging.DEBUG)
+logging.basicConfig(format='%(asctime)s - %(levelname)s: %(message)s.', level=logging.INFO)
 
+# Telegram configs
 TELEGRAM_TOKEN = '';
 ADMIN_CHAT_ID = ''
 
+# Dialogflow configs
 DIALOGFLOW_TOKEN = ''
