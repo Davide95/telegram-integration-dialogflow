39932e70f5c4c389f54abfc15a8bcce632570be9
Davide Riva <driva95@protonmail.com>
2017-12-01 00:55:04 +0100

    Removed unecessary semicolon

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ logging.basicConfig(format='%(asctime)s - %(levelname)s: %(message)s.',
                     level=logging.INFO)
 
 # Telegram configs
-TELEGRAM_TOKEN = '';
+TELEGRAM_TOKEN = ''
 ADMIN_CHAT_ID = ''
 
 # Dialogflow configs
