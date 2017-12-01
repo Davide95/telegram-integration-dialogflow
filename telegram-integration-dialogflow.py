6a15b260d244e9fd8fc06ec5072eb32e7668e25a
Davide Riva <driva95@protonmail.com>
2017-12-01 01:13:07 +0100

    Tabs are replaced with spaces

--- telegram-integration-dialogflow.py
+++ telegram-integration-dialogflow.py
@@ -7,7 +7,7 @@ import logging
 
 import apiai
 from telegram.ext import Updater, CommandHandler, Filters, \
-							MessageHandler, InlineQueryHandler
+    MessageHandler, InlineQueryHandler
 import telegram
 from telegram import InlineQueryResultArticle, InputTextMessageContent
 
@@ -15,53 +15,53 @@ from config import TELEGRAM_TOKEN, ADMIN_CHAT_ID, DIALOGFLOW_TOKEN
 
 
 def start(bot, update):
-	chat_id = update.message.chat_id
-	bot.send_chat_action(chat_id=chat_id, action=telegram.ChatAction.TYPING)
-	reply = dialogflow_event_request('TELEGRAM_WELCOME', chat_id)
-	bot.send_message(chat_id=chat_id, text=reply)
+    chat_id = update.message.chat_id
+    bot.send_chat_action(chat_id=chat_id, action=telegram.ChatAction.TYPING)
+    reply = dialogflow_event_request('TELEGRAM_WELCOME', chat_id)
+    bot.send_message(chat_id=chat_id, text=reply)
 
 
 def text(bot, update):
-	chat_id = update.message.chat_id
-	bot.send_chat_action(chat_id=chat_id, action=telegram.ChatAction.TYPING)
-	reply = dialogflow_text_request(update.message.text, chat_id)
-	bot.send_message(chat_id=chat_id, text=reply)
+    chat_id = update.message.chat_id
+    bot.send_chat_action(chat_id=chat_id, action=telegram.ChatAction.TYPING)
+    reply = dialogflow_text_request(update.message.text, chat_id)
+    bot.send_message(chat_id=chat_id, text=reply)
 
 
 def inline(bot, update):
-	query = update.inline_query.query
-	if not query:
-		return
-	session_id = update.inline_query.from_user.id
-	dialogflow_reply = dialogflow_text_request(query, session_id)
-	reply = list()
-	reply.append(
-	    InlineQueryResultArticle(
-	        id=uuid.uuid4(),
-	        title=query.capitalize(),
-	        input_message_content=InputTextMessageContent(dialogflow_reply),
-	        description=dialogflow_reply
-	    )
-	)
-	bot.answer_inline_query(update.inline_query.id, reply)
+    query = update.inline_query.query
+    if not query:
+        return
+    session_id = update.inline_query.from_user.id
+    dialogflow_reply = dialogflow_text_request(query, session_id)
+    reply = list()
+    reply.append(
+        InlineQueryResultArticle(
+            id=uuid.uuid4(),
+            title=query.capitalize(),
+            input_message_content=InputTextMessageContent(dialogflow_reply),
+            description=dialogflow_reply
+        )
+    )
+    bot.answer_inline_query(update.inline_query.id, reply)
 
 
 def dialogflow_request(request, session_id):
-	request.session_id = session_id
-	response = request.getresponse().read().decode()
-	response_json = json.loads(response, strict=False)
-	return response_json['result']['fulfillment']['messages'][0]['speech']
+    request.session_id = session_id
+    response = request.getresponse().read().decode()
+    response_json = json.loads(response, strict=False)
+    return response_json['result']['fulfillment']['messages'][0]['speech']
 
 
 def dialogflow_event_request(event, session_id):
-	request = DIALOGFLOW.event_request(apiai.events.Event(event))
-	return dialogflow_request(request, session_id)
+    request = DIALOGFLOW.event_request(apiai.events.Event(event))
+    return dialogflow_request(request, session_id)
 
 
 def dialogflow_text_request(query, session_id):
-	request = DIALOGFLOW.text_request()
-	request.query = query
-	return dialogflow_request(request, session_id)
+    request = DIALOGFLOW.text_request()
+    request.query = query
+    return dialogflow_request(request, session_id)
 
 
 logging.info('Program started')
@@ -75,7 +75,7 @@ UPDATER = Updater(token=TELEGRAM_TOKEN)
 DISPATCHER = UPDATER.dispatcher
 logging.info('Bot started')
 if ADMIN_CHAT_ID:
-	BOT.sendMessage(ADMIN_CHAT_ID, text='Bot started.')
+    BOT.sendMessage(ADMIN_CHAT_ID, text='Bot started.')
 
 # Add telegram handlers
 START_HANDLER = CommandHandler('start', start)
@@ -89,5 +89,5 @@ DISPATCHER.add_handler(INLINE_HANDLER)
 UPDATER.start_polling()
 UPDATER.idle()
 if ADMIN_CHAT_ID:
-	BOT.sendMessage(ADMIN_CHAT_ID, text='Program aborted.')
+    BOT.sendMessage(ADMIN_CHAT_ID, text='Program aborted.')
 logging.info('Program aborted')
